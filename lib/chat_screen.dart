487abd8c2646cd532266fe1cf54ba4510cc7ac71
ARTHUR DONIZETI RODRIGUES DIAS <arthur.dias4@fatec.sp.gov.br>
2020-12-06 18:51:33 -0300

    first commit

---
+++ lib/chat_screen.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:app_chatbot/text_composer.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  Future<void> _sendMessage({String text, File imgFile}) async {
+    Map<String, dynamic> data = {};
+
+    if (imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().microsecondsSinceEpoch.toString())
+          .putFile(imgFile);
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data["imgUrl"] = url;
+      print(url);
+    }
+    if (text != null) data['text'] = text;
+    Firestore.instance.collection('messages').add(data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Olá'),
+        elevation: 0,
+      ),
+      body: TextComposer(_sendMessage),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:app_chatbot/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.blue),
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
